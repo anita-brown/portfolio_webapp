6ceb5bcdb3caa4aac1cb7631975bcbc0190280bd
anita-brown <efeaniita@gmail.com>
2022-12-27 23:22:35 +0100

    chore: created hero section

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,13 +1,25 @@
-import React from 'react';
+import React from "react";
 
-import { Section, SectionText, SectionTitle } from '../../styles/GlobalComponents';
-import Button from '../../styles/GlobalComponents/Button';
-import { LeftSection } from './HeroStyles';
+import { Section, SectionText, SectionTitle } from "../../styles/GlobalComponents";
+import Button from "../../styles/GlobalComponents/Button";
+import { LeftSection } from "./HeroStyles";
 
 const Hero = (props) => (
-  <div>
-    Hero
-  </div>
+  <>
+    <Section row nopadding>
+      <LeftSection>
+        <SectionTitle main center>
+          Hi, My name is Anita, <br /> Welcome To <br />
+          My Personal Portfolio
+        </SectionTitle>
+        <SectionText>
+          I’m a Frontend Developer with 3yrs of experience in working with Typescript, Git, CSS, SASS and Javascript frameworks such as React and Next in building web applications and consuming backend API. Contributing to teams success with agile
+          methodology and grasping new concepts quickly are two of my biggest strengths. i am a critical thinker and a good communicator, who is eager to learn new things and overcome new challenges.
+        </SectionText>
+        <Button onClick={() => (window.location = "https://mail.google.com/mail/u/0/#inbox")}>Learn More</Button>
+      </LeftSection>
+    </Section>
+  </>
 );
 
 export default Hero;
